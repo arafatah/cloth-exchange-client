baa9c103437441e9842e7117ee29dd021ca68654
arafatah <arafathowlader456@gmail.com>
2023-11-05 21:52:44 +0600

    cart added succecfully

---
+++ src/page/ShowCart/ServiceCart.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ServiceCart = ({ service }) => {
+console.log(service);
+  return (
+    <div className="bg-white rounded-lg shadow-md w-64 p-4 m-4">
+      <img
+        src={service.image} // Replace with your service's image source
+        alt={service.serviceName}
+        className="w-full h-32 object-cover object-center rounded-md"
+      />
+      <h2 className="text-xl font-semibold mt-4">{service.serviceName}</h2>
+      <p className="text-gray-500 mt-2">{service.description}</p>
+      <div className="mt-4">
+        <p className="text-xl font-semibold text-blue-600">${service.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCart;
--- src/page/ShowCart/ShowCart.jsx
+++ src/page/ShowCart/ShowCart.jsx
@@ -1,25 +1,30 @@
 import { useEffect, useState } from "react";
-
+import ServiceCart from "./ServiceCart";
 
 const ShowCart = () => {
- const [cart, setCart] = useState([]);
-    useEffect(()=>{
-        fetch('http://localhost:5000/orders')
-        .then(res => res.json())
-        .then(data => {
-            setCart(data);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }
-    ,[])
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/orders")
+      .then((res) => res.json())
+      .then((data) => {
+        setCart(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-        <div>
-            {cart.length}
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap justify-center">
+      {cart.length === 0 ? (
+        <p>You have no service at cart</p>
+      ) : (
+        cart.map((service) => (
+          <ServiceCart key={service.id} service={service} />
+        ))
+      )}
+    </div>
+  );
 };
 
 export default ShowCart;
